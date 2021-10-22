21acc0773489366a11d4451f0ed18553689bc023
TaeWoonJeong <pig07911@naver.com>
2021-10-22 23:29:56 +0900

    init project

---
+++ src/test/kotlin/com/opensource/seebus/SeebusApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.opensource.seebus
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SeebusApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
